a34031498cd3ab82e09884a5907b717b1d57d58b
Jon Holderman <29864900+jonholderman@users.noreply.github.com>
2021-05-18 08:43:30 -0400

    Create SetAssignmentGroupAndDescription.js

---
+++ Scripts/Server/Workflow/SetAssignmentGroupAndDescription.js
@@ -0,0 +1,16 @@
+// Assign Assignment Group to 'T2 AGI Technical Support' since this is an AGI Task
+task.assignment_group.name = 'ServiceNow Group Name';
+
+// If the variable 'New Folder' is selected for the 'folderType' Variable  
+if(current.variables.folderType == 'New Folder') {
+	
+	// Set the Short Description as 'New Restricted Shared Folder'
+	task.short_description = 'New Restricted Shared Folder';
+
+// If the variable 'Increase Size of Existing Folder' is selected for the 'folderType' Variable  
+} else if(current.variables.folderType == 'Increase Size of Existing Folder') {
+	
+	// Set the Short Description as 'Increase Size of Existing Shared Folder'
+	task.short_description = 'Increase Size of Existing Shared Folder';
+	
+}
